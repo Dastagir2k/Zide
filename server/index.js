4d8311a268160274abf4d2a1dba7c4d8f0d57ef5
unknown <dastagir2k@gmail.com>
2024-11-28 19:35:51 +0530

    db

--- server/index.js
+++ server/index.js
@@ -141,7 +141,7 @@ app.post("/compile", async (req, res) => {
 // get the code using user'id
 app.get("/getcode",async(req,res)=>{
     const userId = req.query.userId;
-    const response=await Code.find({userId:userId}).explain("executionStats")
+    const response=await Code.find({userId:userId})
 
     for (lastElement in response);
     lastElement;
